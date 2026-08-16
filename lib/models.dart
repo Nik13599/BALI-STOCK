b933ef7c370c8a319dc1a7a839badfbec6376cdc
Nik13599 <KOLYASELEKH@gmail.com>
2026-08-16 23:54:53 +0300

    Represent initial stocktake history correctly

--- lib/models.dart
+++ lib/models.dart
@@ -78,6 +78,7 @@ class StockOperationLine {
     required this.categoryName,
     required this.bottleMl,
     required this.beforeTotalMl,
+    required this.beforeInitialized,
     required this.changeTotalMl,
     required this.afterTotalMl,
   });
@@ -87,6 +88,7 @@ class StockOperationLine {
   final String categoryName;
   final int bottleMl;
   final int beforeTotalMl;
+  final bool beforeInitialized;
   final int changeTotalMl;
   final int afterTotalMl;
 }
