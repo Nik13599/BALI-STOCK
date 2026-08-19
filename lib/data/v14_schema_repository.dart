3ecfb888552b0440aaa94b047c95a01311e7a980
Nik13599 <KOLYASELEKH@gmail.com>
2026-08-19 15:41:21 +0300

    deduplicate V14 SQLite migration schema

--- lib/data/v14_schema_repository.dart
+++ lib/data/v14_schema_repository.dart
@@ -1,4 +1,5 @@
 import 'database.dart';
+import 'operation_schema.dart';
 
 class V14SchemaRepository {
   V14SchemaRepository({BaliStockDatabase? database}) : _database = database ?? BaliStockDatabase.instance;
@@ -17,50 +18,7 @@ class V14SchemaRepository {
         await txn.execute('ALTER TABLE operation_lines RENAME TO operation_lines_pre_v14');
         await txn.execute('ALTER TABLE operations RENAME TO operations_pre_v14');
 
-        await txn.execute('''
-          CREATE TABLE operations (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            type TEXT NOT NULL CHECK (type IN ('delivery', 'stocktake', 'spot_stocktake', 'writeoff', 'transfer', 'correction')),
-            created_at TEXT NOT NULL,
-            employee_name TEXT,
-            started_at TEXT,
-            completed_at TEXT,
-            active_seconds INTEGER NOT NULL DEFAULT 0,
-            total_seconds INTEGER NOT NULL DEFAULT 0,
-            supplier_id TEXT,
-            supplier_name TEXT,
-            document_number TEXT,
-            comment TEXT,
-            attachment_url TEXT,
-            source_location_id TEXT,
-            source_location_name TEXT,
-            target_location_id TEXT,
-            target_location_name TEXT,
-            correction_of TEXT,
-            total_value REAL
-          )
-        ''');
-        await txn.execute('''
-          CREATE TABLE operation_lines (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            operation_id INTEGER NOT NULL,
-            product_id INTEGER NOT NULL,
-            product_name TEXT NOT NULL,
-            category_name TEXT NOT NULL,
-            bottle_ml INTEGER NOT NULL,
-            stock_unit TEXT NOT NULL DEFAULT 'ml',
-            before_total_ml INTEGER NOT NULL,
-            before_initialized INTEGER NOT NULL DEFAULT 1,
-            change_total_ml INTEGER NOT NULL,
-            after_total_ml INTEGER NOT NULL,
-            unit_cost REAL,
-            line_value REAL,
-            comment TEXT,
-            source_location_id TEXT,
-            target_location_id TEXT,
-            FOREIGN KEY(operation_id) REFERENCES operations(id) ON DELETE CASCADE
-          )
-        ''');
+        await createOperationTables(txn);
 
         await txn.execute('''
           INSERT INTO operations(
